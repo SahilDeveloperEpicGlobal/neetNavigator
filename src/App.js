f5de8a162b6b2c33d352f21972cca97ac85d4265
Krishna Kumar <krishnakmar555@gmail.com>
2023-02-07 15:27:21 +0530

    changes from krishna end

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
@@ -5,8 +6,8 @@ import "./App.css";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-// eslint-disable-next-line no-unused-vars
-import { FormControl, InputLabel, Link, MenuItem, Select, TextField } from "@mui/material";
+
+import { Link } from "@mui/material";
 import InstagramIcon from '@mui/icons-material/Instagram';
 function App() {
   return (
@@ -65,12 +66,277 @@ function App() {
   <h2>You need to make sure that your CRS score is above the minimum points score of your <Link href="#">
     <a>round of invitations</a></Link>.</h2>
     <h3>1.Student Registration</h3>
-    <FormControl fullWidth>
+    <form>
       <label>Student Name</label>
       <input type={"text"} placeholder={"Enter your full name"}/>
-</FormControl>
+      <label>Select your Gender</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Enter your Mobile Number</label>
+      <input type={"Number"} placeholder={"10 Digit number"}/>
+      <label>Verify mobile number</label>
+      <p>A 6 digit code has been sent to:<br></br>
+<strong>95987654678</strong></p>
+<label className="emailinput">Enter your Email Address</label>
+      <input type={"Email"} />
+      <label >Enter the verification code here</label>
+      <input type={"code"} />
+      <p><Link href={"#"}><a>Didn't get the code?</a></Link> </p>
+      <ul>
+        <li><Link href={"#"}><a>Resend on SMS</a></Link></li>
+        <li><Link href={"#"}><a>Resend on call</a></Link></li>
+      </ul>
+      <h3>2.Enter your Domicile</h3>
+      <label>[Senior Secondary] 12th Passing State</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>[High School] 10th Passing State</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Permanent Address Postal Code</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label >State</label>
+      <input type={"code"} />
+      <label >City</label>
+      <input type={"code"} />
+      <label >City</label>
+      <input type={"code"} />
+      <h3>3.Enter your Category</h3>
+      <label>Whether Physically handicapped Candidate?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Whether Belongs to Armed Force?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Whether Orphan or Not?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Whether Single Child or Not?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Whether belongs to minority?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<label>Whether eligible for Quota?</label>
+<select name="" id="students">
+  <option value="volvo">Select</option>
+  <option value="saab">1</option>
+  <option value="mercedes">2</option>
+  <option value="audi">3</option>
+</select>
+<h3 className="lastinput">4.Enter your Marks</h3>
+<input type={"code"} />
+<p className="markpara">Your estimated rank between 1 lakh to 1,50 lakh</p>
+<input type={"submit"} value={"Calculate your score"}></input>
+</form>
 </div>
  </div>
+</div>
+<div className="section4">
+ <div className="container">
+ <p>A college availability graph can be used to measure the availability of college courses for a given semester. This graph can be used to show the availability of courses offered by a particular college or university, as well as the availability of courses in a specific subject area.</p>
+ <h2>Data Analysis's of NEET Predictor </h2>
+ <div className="neetable">
+  <ul>
+    <li className="heading">Time/Date</li>
+    <li>24-01-2022 [14:08]  </li>
+    <li>24-01-2022 [14:08]  </li>
+    <li>24-01-2022 [14:08]  </li>
+    <li>24-01-2022 [14:08]  </li>
+    <li>24-01-2022 [14:08]  </li>
+    <li>24-01-2022 [14:08]  </li>
+  </ul>
+  <ul>
+    <li className="heading">No. of College</li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+  </ul>
+  <ul>
+    <li className="heading">College Type</li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+    <li>4 College  </li>
+  </ul>
+  <ul>
+    <li className="heading">State</li>
+    <li>IN/Punjab</li>
+    <li>IN/Haryana  </li>
+    <li>IN/Bangalore</li>
+    <li>IN/Punjab</li>
+    <li>IN/Haryana</li>
+    <li>IN/Bangalore</li>
+  </ul>
+ </div>
+ <p><strong>NOTE: </strong>The contents of a college admission list will vary depending on the college and the type of admissions process. Generally, a college admission list will include information about each student who has been accepted to the college, such as their name, address, contact information, test scores, and other admissions criteria that were used to make the decision. Depending on the college, the list may also include the student's major, any scholarships or financial aid that have been awarded, and any special programs or services that the student has been accepted into</p>
+ </div>
+</div>
+<div className="section5">
+<div className="container">
+<h2>College List</h2>
+<table>
+  <tr>
+    <th>State</th>
+    <th>No. of College</th>
+    <th className="countrynest3">Select the Colleges
+    <table className="countrynest2">
+      <tr>
+        <th>
+          <input type={"checkbox"} />
+        </th>
+        <th>Gov.</th>
+        <th>Pvt.</th>
+        <th>Dem.</th>
+      </tr>
+    </table>
+    </th>
+  </tr>
+  <tr>
+    <td>Punjab</td>
+    <td>30 College</td>
+    <td>
+    <table>
+    <tr>
+    <td className="countrynest">   <input type={"checkbox"} /></td>
+    <td className="countrynest">34</td>
+        <td className="countrynest">56</td>
+        <td className="countrynest">23</td>
+      </tr>
+    </table>
+      </td>
+  </tr>
+  <tr>
+    <td>Bangalore</td>
+    <td>78 College</td>
+    <td>
+    <table>
+    <tr>
+    <td className="countrynest">   <input type={"checkbox"} /></td>
+    <td className="countrynest">34</td>
+        <td className="countrynest">56</td>
+        <td className="countrynest">23</td>
+      </tr>
+    </table>
+      </td>
+  </tr>
+  <tr>
+    <td>Bhopal</td>
+    <td>45 College</td>
+    <td>
+    <table>
+    <tr>
+    <td className="countrynest">   <input type={"checkbox"} /></td>
+    <td className="countrynest">34</td>
+        <td className="countrynest">56</td>
+        <td className="countrynest">23</td>
+      </tr>
+    </table>
+      </td>
+  </tr>
+  <tr>
+    <td>Haryana</td>
+    <td>19 College</td>
+    <td>
+    <table className="countrynest1">
+    <tr>
+        <td className="countrynest">   <input type={"checkbox"} /></td>
+        <td className="countrynest">34</td>
+        <td className="countrynest">56</td>
+        <td className="countrynest">23</td>
+      </tr>
+    </table>
+      </td>
+  </tr>
+</table>
+</div>
+</div>
+<div className="section6">
+<div className="container">
+  <div className="maincart">
+  <div className="cartsummary">
+    <h3>Cart Summary</h3>
+<ul>
+  <li> <input type={"radio"} />
+  All states </li><li>ufgdu</li>
+</ul>
+<ul>
+  <li> <input type={"radio"} />
+  1 state</li>
+ <li>ufgdu</li>
+</ul>
+<ul>
+  <li> <input type={"radio"} />
+  2 state</li>
+ <li>ufgdu</li>
+</ul>
+<ul>
+  <li> <input type={"radio"} />
+  2 marks</li>
+ <li>ufgdu</li>
+</ul>
+<ul>
+  <li> <input type={"radio"} />
+  4 marks</li>
+ <li>ufgdu</li>
+</ul>
+<ul>
+  <li className="subtotal">
+  SUBTOTAL</li>
+ <li>ufgdu</li>
+</ul>
+<button>Checkout</button>
+  </div>
+  <div className="summarycontent">
+    <p>In order to find out which colleges accept applicants with good credit scores, you should contact each school directly to inquire about their admissions policies and what kind of credit score they look for. Additionally, you can look for colleges that are known for being “credit friendly,” which means that they don’t require a credit check as part of their admissions process. Finally, you can research scholarship and grant programs that are specifically designed for students with good credit scores.</p>
+  </div>
+  <div className="summarygraph">
+    fsdafcxersdadfc
+  </div>
+  </div>
+</div>
 </div>
     </> 
   );
